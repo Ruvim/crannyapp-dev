7d093e161fda01fd80a15f9b0ea9430d8a984806
Rutvi Maniar <rutvi@anilabsinc.com>
2016-02-29 12:58:48 +0530

    Merge branch 'master' of https://github.com/Ruvim/crannyapp-dev
    # Conflicts:
    #	README.md

--- application/config/google.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $config['google']['application_name'] = 'crannyapp';
-    $config['google']['client_id'] = '726081332676-ce8vmntbbrbjj97sdh263qmkqd89vgh6.apps.googleusercontent.com';
-    $config['google']['client_secret'] = 'O80cEqoyNzAYUqJYrTM4ZAyZ';
-    $config['google']['redirect_uri'] = 'https://crannyapp.herokuapp.com/admin/googlelogin';
-    $config['google']['api_key'] = 'AIzaSyCWjvlJKqVvOIPlaE-OMfUrPz14ESK5N_s'; 
-?>
